d11b67c1bfba37ca2e34fc80e50c9d93fbbaf56b
MIdnightfury <31964173+MIdnightfury@users.noreply.github.com>
2021-04-12 00:26:43 -0400

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const switcher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function () {
-    window.location.href = "https://media.tenor.co/videos/1fef4b37b9b9e721d49ba6d6b8fdacb9/mp4"
+    window.location.href = "https://www.youtube.com/watch?v=s-n9TaTdOjA&t=2s"
 });
 /*
 switcher.addEventListener('mouseenter', function (event) {
@@ -11,8 +11,8 @@ switcher.addEventListener('mouseenter', function (event) {
 })*/
 const tooltip = document.querySelector('.tooltip')
 switcher.addEventListener('mouseenter', function (event) {
-    switcher.style.left = getRandomArbitrary(0,50).toString() + "%"
-    switcher.style.top = getRandomArbitrary(0,50).toString() + "%"
+    switcher.style.left = getRandomArbitrary(0,100).toString() + "%"
+    switcher.style.top = getRandomArbitrary(0,100).toString() + "%"
 })
 switcher.addEventListener('mouseleave', function (event) {
     switcher.src=""
