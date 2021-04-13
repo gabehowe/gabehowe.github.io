f1c6c70d46824f20113ef10bf1a5ab4b14be8a35
MIdnightfury <31964173+MIdnightfury@users.noreply.github.com>
2021-04-12 22:57:59 -0400

    update

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 'use strict'
-const switcher = document.querySelector('.btn');
-const roasts = ["smh imagine not being able to click this","you're just bad aren't you",
-"u mum", "no u", "nub", "lol imagine not being able to click this"]
-switcher.addEventListener('click', function () {
-    window.location.href = "https://www.youtube.com/watch?v=s-n9TaTdOjA&t=2s"
+const button = document.body.querySelector('.btn');
+const roasts = ["smh imagine not being able to click this", "you're just bad aren't you",
+    "u mum", "no u", "nub", "lol imagine not being able to click this"]
+button.addEventListener('click', function () {
+    button.style.visibility = 'hidden'
+    document.title = "Second Trial"
+    const title = document.body.querySelector('.title')
+    title.style.visibility = 'visible'
+    title.innerHTML = "Well"
+    setTimeout(() => { title.innerHTML = "You got past the first trial" }, 2000);
 });
-/*
-switcher.addEventListener('mouseenter', function (event) {
-    console.log("i been hovered :\'(")
-    window.location.href = "https://media.tenor.co/videos/1fef4b37b9b9e721d49ba6d6b8fdacb9/mp4"
-})*/
-const tooltip = document.querySelector('.tooltip')
-switcher.addEventListener('mouseenter', function (event) {
-    switcher.innerHTML = roasts[getRandomArbitrary(0,roasts.length)]
-    switcher.style.left = getRandomArbitrary(0,100).toString() + "%"
-    switcher.style.top = getRandomArbitrary(0,100).toString() + "%"
-})
-switcher.addEventListener('mouseleave', function (event) {
-    switcher.src=""
-})
+// button.addEventListener('mouseenter', function (event) {
+//     button.innerHTML = roasts[getRandomArbitrary(0, roasts.length)]
+//     button.style.left = getRandomArbitrary(5, 95).toString() + "%"
+//     button.style.top = getRandomArbitrary(5, 95).toString() + "%"
+// })
 
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
