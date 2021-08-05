37da3decba302601ea1964b512259b9395e4c185
MIdnightfury <31964173+MIdnightfury@users.noreply.github.com>
2021-08-04 21:21:11 -0400

    keybinds

--- hotbartrainer/hotkeytrainer.js
+++ hotbartrainer/hotkeytrainer.js
@@ -6,13 +6,46 @@
  * the you have the app randomly show you an item and the time it takes for you to switch to it
  * sort of like a touch typing trainer specifically for minecraft hotbars***/
 const wool = document.body.querySelector(".rainbow_wool")
+const binds = {}
+let nextKey = false;
+let toBeBound = '';
+const bottomText = document.getElementById('bottomText')
+const topText = document.getElementById('topText')
 document.addEventListener('keypress', (event) => {
+    if (event.shiftKey && !nextKey) {
+        nextKey = true
+        toBeBound = event.key
+        return;
+    }
+    if (nextKey) {
+        if (isNaN(event.key)) {
+            topText.style.color = 'rgba(255,0,21,0.68)'
+            topText.innerText = "invalid slot number"
+            setTimeout(() => {
+                topText.innerText = ''
+            }, 1000)
+            return;
+        }
+        if (bottomText.innerText.includes(`: ${toBeBound}`)) {
+            const num = bottomText.innerText.substr(bottomText.innerText.indexOf(` : ${toBeBound}`) - 1, 1)
+            binds[num] = undefined
+        }
+        binds[event.key] = toBeBound
+        let boundstr = "use shift + &lt;key&gt; and then the number of the slot to bind a key <br> Current binds: <br>"
+        for (const [key, value] of Object.entries(binds)) {
+            if (value === undefined) continue
+            boundstr += `${key} : ${value} <br>`
+        }
+        bottomText.innerHTML = boundstr
+        console.log(`${toBeBound} bound to ${event.key}`)
+        nextKey = false
+    }
     if (isNaN(event.key)) {
         return
     }
     if (event.key === '0') {
         return;
     }
-    document.documentElement.style.setProperty("--magic_number_mult",  event.key - 5)
+    // document.documentElement.style.setProperty("--magic_number_mult",  event.key - 5)
 
 })
